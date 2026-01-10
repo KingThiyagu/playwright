9a352e62447845542177f793ed08dc6c74436b4c
king1910 <104858776+KingThiyagu@users.noreply.github.com>
2026-01-10 13:37:16 +0530

    Clean up fileA.spec.js by removing commented linestest

--- tests/fileA.spec.js
+++ tests/fileA.spec.js
@@ -1,10 +1,4 @@
 //fileA new file created
 console.log('This is file A test file.');
-<<<<<<< HEAD
-//line 356
-//line 3
-=======
 //line 5
 //line 3
-//line 4
->>>>>>> 91538411c2e241161c9472d4717ed1f6e04d195d
